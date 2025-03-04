f4c2e2e0e3ec648c5024cfe57268c8905a86e712
j-e-ff <118487175+j-e-ff@users.noreply.github.com>
2025-03-03 21:58:16 -0800

    Create knn.py

---
+++ assignent2/knn.py
@@ -0,0 +1,75 @@
+#-------------------------------------------------------------------------
+# AUTHOR: your name
+# FILENAME: title of the source file
+# SPECIFICATION: description of the program
+# FOR: CS 4210- Assignment #2
+# TIME SPENT: how long it took you to complete the assignment
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
+
+#Importing some Python libraries
+from sklearn.neighbors import KNeighborsClassifier
+import csv
+
+db = []
+
+#Reading the data in a csv file
+with open('email_classification.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0: #skipping the header
+         db.append(row)
+
+#mapping the labels to numbers
+labels = {'ham': 0, 'spam': 1}
+error_count = 0
+
+#Loop your data to allow each instance to be your test set
+for i in range(len(db)):
+    #Add the training features to the 20D array X removing the instance that will be used for testing in this iteration.
+    #For instance, X = [[1, 2, 3, 4, 5, ..., 20]].
+    #Convert each feature value to float to avoid warning messages
+    #--> add your Python code here
+    X = []
+    Y = []
+
+    #Transform the original training classes to numbers and add them to the vector Y.
+    #Do not forget to remove the instance that will be used for testing in this iteration.
+    #For instance, Y = [1, 2, ,...].
+    #Convert each feature value to float to avoid warning messages
+    #--> add your Python code here
+    for j in range(len(db)):
+        if j != i:
+            X.append([float(value) for value in db[j][:-1]])
+            Y.append(labels[db[j][-1]])
+
+    #Store the test sample of this iteration in the vector testSample
+    #--> add your Python code here
+    testSample = [float(value) for value in db[i][:-1]]
+    true_label = labels[db[i][-1]]
+
+    #Fitting the knn to the data
+    clf = KNeighborsClassifier(n_neighbors=1, p=2)
+    clf = clf.fit(X, Y)
+
+    #Use your test sample in this iteration to make the class prediction. For instance:
+    #class_predicted = clf.predict([[1, 2, 3, 4, 5, ..., 20]])[0]
+    #--> add your Python code here
+    class_predicted = clf.predict([testSample])[0]
+
+    #Compare the prediction with the true label of the test instance to start calculating the error rate.
+    #--> add your Python code here
+    if class_predicted != true_label:
+        error_count += 1
+
+#Print the error rate
+#--> add your Python code here
+error_rate = error_count/len(db)
+print(f'Error Rate: {error_rate}')
+
+
+
+
+
+
